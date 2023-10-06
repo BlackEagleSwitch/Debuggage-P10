743c9e131038829089e245a33357ecf1f10bce42
Corentin Beaudet <corentin.abbruzzese14@gmail.com>
2023-10-06 17:39:42 +0200

    finish project 10

---
+++ src/containers/Form/index.test.js
@@ -0,0 +1,37 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Form from "./index";
+
+describe("When Events is created", () => {
+    it("a list of event card is displayed", async () => {
+        render(<Form />);
+        await screen.findByText("Email");
+        await screen.findByText("Nom");
+        await screen.findByText("Prénom");
+        await screen.findByText("Personel / Entreprise");
+    });
+
+    describe("and a click is triggered on the submit button", () => {
+        it("the success action is called", async () => {
+            
+            const onSuccess = jest.fn();
+            
+            render(<Form />);
+
+            fireEvent(
+                await screen.findByText("Envoyer"),
+                new MouseEvent("click", {
+                    cancelable: true,
+                    bubbles: true,
+                })
+            );
+            
+            await screen.findByText("En cours");
+
+            setTimeout(async () => {
+                expect(onSuccess).toHaveBeenCalled();
+
+                await screen.findByText("Message envoyé !");
+            }, 1005)            
+        });
+    });
+});
